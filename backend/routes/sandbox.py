97f245cb91bc44ac7bd1dd7fc9799cbce0fc4711
Dragos Manolea <dragosmanolea@gmail.com>
2023-03-11 20:15:23 +0200

    file path in formData

--- backend/routes/sandbox.py
+++ backend/routes/sandbox.py
@@ -7,6 +7,9 @@ from lib.mongo_client import pomodoro_db
 
 sandbox_blueprint = Blueprint("sandbox", __name__, url_prefix="/api/v1/sandbox")
 sandbox_collection = pomodoro_db['sandbox']
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 client = docker.from_env()
 @sandbox_blueprint.route("/start", methods=["POST"])
 def run_sandbox():
@@ -18,36 +21,37 @@ def run_sandbox():
     description: "Endpoint to start a sandbox"
     summary: "Endpoint to start a sandbox"
     parameters:
-      - name: "body"
-        in: "body"
-        required: true
+      - in: formData
+        name: file_path
+        type: string
     responses:
       200:
         description: "A new endpoint was created was created"
       400:
         description: "generic error"
     """
-    try:
-        print(f'request data is: {request.data}')
-        print(request.data)
-        print(f'starting container...')
-        container = client.containers.run(
-            'sandbox:latest',  # replace with your image name and tag
-            detach=True,  # run container in the background
-            volumes={
-                '/sandbox/main.py': {
-                    'bind': '/usr/dummy.py',
-                    'mode': 'rwx'
-                }
-            }
-        )
 
-        # get the container ID
-        container_id = container.id
-
-        # print the container ID
-        print(f'Container ID: {container_id}')
-    except Exception as e:
-        return "An error has occurred: {}".format(str(e)), 400
+    # try:
+    logging.debug(f'request form is: {request.form}')
+    # print(request.data)
+    # print(f'starting container...')
+    # container = client.containers.run(
+    #     'sandbox:latest',  # replace with your image name and tag
+    #     detach=True,  # run container in the background
+    #     volumes={
+    #         '/sandbox/main.py': {
+    #             'bind': '/usr/dummy.py',
+    #             'mode': 'rwx'
+    #         }
+    #     }
+    # )
+    #
+    # # get the container ID
+    # container_id = container.id
+    #
+    # # print the container ID
+    # print(f'Container ID: {container_id}')
+    # # except Exception as e:
+    # #     return "An error has occurred: {}".format(str(e)), 400
 
     return jsonify({'status': 'ok'}), 201
--- sandbox/main.py
+++ sandbox/main.py
@@ -1,6 +1,3 @@
-# import time
-# time.sleep(10000)
-
 if __name__ == "__main__":
     print('hello from sandbox')
     # print "a"
