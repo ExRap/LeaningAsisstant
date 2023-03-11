8cbe92b9728e729a52fb138b3829efcbef336681
Dragos Manolea <dragosmanolea@gmail.com>
2023-03-11 18:09:12 +0200

    create sandbox

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,2 +1,6 @@
-print "a"
-print('hello from sandbox')
+# import time
+# time.sleep(10000)
+
+if __name__ == "__main__":
+    print('hello from sandbox')
+    # print "a"
--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm sandbox
+docker run -v /Users/dragosmanolea/work/LeaningAsisstant/sandbox/main.py:/usr/dummy.py -it sandbox $1
