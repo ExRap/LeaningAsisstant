c71ccd93d86937c3ab8162551b7c4c0c1aaa6971
Gheorghita Caulea <gheorghita@192-168-0-175.rdsnet.ro>
2023-03-12 11:42:46 +0200

    Final UI version for demo + sdk updated

--- genezio/server/gptCaller.js
+++ genezio/server/gptCaller.js
@@ -65,24 +65,19 @@ export class GptCaller {
 
     // create a POST request and add it to mongodb
     // TODO
+    const postData = JSON.stringify({
+      file_content: JSON.stringify({uid: user_id,
+      requestText: requestText})
+  });
+		
+		makePostRequest('http://hackathon.echopoint.tech/api/v1/partial/', postData)
+			.then((data) => {
+				console.log(data)
+			})
+			.catch((err) => {
+			console.error(err);
+			});
 
-    fetch("http://hackathon.echopoint.tech/api/v1/partial/", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: {
-        'uid': user_id,
-        'requestText': requestText
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error(error);
-    });
 
     let lines = completion.data.choices[0].text.split("\n");
     lines = lines.splice(0, lines.length / 2);
@@ -91,32 +86,66 @@ export class GptCaller {
   }
 
   async generateCompleteFunction(user_id, requestText) {
-    // create a GET request to get it from mongodb
-    fetch(`http://hackathon.echopoint.tech/api/v1/partial${user_id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      return data;
-    })
-    .catch(error => {
-      console.error(error);
-    });
-    const completion = await this.openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Write a Python function` + requestText,
-      max_tokens: 2048
-    });
-    console.log(
-      `DEBUG: request: ${requestText}, response: ${completion.data.choices[0]
-        .text}`
-    );
-    return completion.data.choices[0].text;
+    fetchData('http://hackathon.echopoint.tech/api/v1/partial/${user_id}')
+    .then((data) => {
+              console.log(data.requestText)
+              return data.requestText
+          })
+        .catch((err) => {
+            console.error(err);
+            });
   }
 
 
+}
+const https = require('http');
+
+function makePostRequest(url, postData) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(postData),
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(url, options, (res) => {
+      let body = '';
+
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(body);
+      });
+    }).on('error', (err) => {
+      reject(err);
+    });
+
+    req.write(postData);
+    req.end();
+  });
+}
+
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+      response.on('end', () => {
+        try {
+          const parsedData = JSON.parse(data);
+          resolve(parsedData);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }).on('error', (error) => {
+      reject(error);
+    });
+  });
 }
